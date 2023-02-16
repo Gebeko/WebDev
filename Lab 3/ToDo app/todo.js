b5051bf0702f1bfc4c63a72ddd84af3ced950c9b
Gebeko <99469894+Gebeko@users.noreply.github.com>
2023-02-16 21:59:45 +0600

    1

---
+++ Lab 3/ToDo app/todo.js
@@ -0,0 +1,43 @@
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+var list = document.querySelector('ul');
+list.addEventListener('change', function(myCheckBox) {
+    if (this.checked) {
+        myCheckBox.target.classList.toggle('checked');
+    }
+});
+function newElement(){
+    let item = document.createElement('li');
+    let inputVal = document.getElementById("userInput").value;
+    let temp = document.createTextNode(inputVal);
+    let checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.value = 1;
+        checkbox.name = "todo[]";
+    item.appendChild(checkbox);
+    item.appendChild(temp);
+    if(inputVal===''){
+        alert("Write something!!!");
+    }
+    else{
+        document.getElementById("myUL").appendChild(item);
+    }
+    document.getElementById("userInput").value="";
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("🗑");
+    span.className = "close";
+    span.appendChild(txt);
+    item.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
